e7c395dc78057ad754af4def9497c4ec08f1ba23
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2020-04-21 11:51:21 -0300

    Styled components, Repositories layout styling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,44 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/14045736?s=460&u=7b0a45bf18f5340dee90cf5bfb13804a9d534d8e&v=4"
+            alt="Victor Hugo Araujo"
+          />
+          <div>
+            <strong>gostack-desafios-typeorm-upload</strong>
+            <p>Upload csv files</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/14045736?s=460&u=7b0a45bf18f5340dee90cf5bfb13804a9d534d8e&v=4"
+            alt="Victor Hugo Araujo"
+          />
+          <div>
+            <strong>gostack-desafios-typeorm-upload</strong>
+            <p>Upload csv files</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/14045736?s=460&u=7b0a45bf18f5340dee90cf5bfb13804a9d534d8e&v=4"
+            alt="Victor Hugo Araujo"
+          />
+          <div>
+            <strong>gostack-desafios-typeorm-upload</strong>
+            <p>Upload csv files</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
